cd6c0b3ac385d60d5368f3e78ae83af03c8b17d2
hassoonsy2 <71430169+hassoonsy2@users.noreply.github.com>
2021-03-24 14:41:02 +0100

    Simpel recommendion done

--- front-end/huw_recommend.py
+++ front-end/huw_recommend.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource, reqparse
 import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
+import psycopg2
 
 app = Flask(__name__)
 api = Api(app)
@@ -21,7 +22,26 @@ if os.getenv(envvals[0]) is not None:
     client = MongoClient(dbstring.format(*envvals))
 else:
     client = MongoClient()
-database = client.huwebshop 
+database = client.huwebshop
+
+def connect():
+    """This function is the connection with the postgres db"""
+    global connection
+    connection = psycopg2.connect(host='localhost', database='huwebshop', user='postgres', password='Xplod_555')
+    return connection
+
+def sql_select(sql):
+    """This function select values from the tables on the Postgres db"""
+    c = connect()
+    cur = c.cursor()
+    cur.execute(sql)
+    results = cur.fetchall()
+    return results
+
+def commit():
+   """This function will cpmmit  a query on the Postgres db """
+   c = connect()
+   c.commit()
 
 class Recom(Resource):
     """ This class represents the REST API that provides the recommendations for
@@ -32,8 +52,22 @@ class Recom(Resource):
         """ This function represents the handler for GET requests coming in
         through the API. It currently returns a random sample of products. """
         randcursor = database.products.aggregate([{ '$sample': { 'size': count } }])
-        prodids = list(map(lambda x: x['_id'], list(randcursor)))
+        prodids = self.simpel_reco()
         return prodids, 200
+
+    def simpel_reco(self):
+        id_lists = []
+        result = sql_select(""" SELECT prodid , name
+            FROM BEST_seller
+            WHERE Counter > 1000
+            LIMIT 4;""")
+        commit()
+        for elment in result :
+            id_lists.append(elment[0])
+        return id_lists
+
+
+
 
 # This method binds the Recom class to the REST API, to parse specifically
 # requests in the format described below.
